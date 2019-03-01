c38f21afa0202a11dce46b64d1962827bec8e171
Jeppe Vennekilde <jeppe@jeppes.info>
2019-03-01 23:56:33 +0100

    Added method for skipping tokeninfo api call when changing the api key
    This is done, as there is a rate limit of 600 calls per minute. Hence no reason to waste a lot on checking an api key is available, when we already have the data persisted in the database

--- pkg/gw2api/gw2api.go
+++ pkg/gw2api/gw2api.go
@@ -15,21 +15,22 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strings"
 	"time"
 )
 
 // GW2Api is the state holder of the API. It includes authentication information
 type GW2Api struct {
-	timeout   time.Duration
-	client    http.Client
-	auth      string
-	authFlags uint
+	Timeout   time.Duration
+	Client    http.Client
+	Auth      string
+	AuthFlags uint
 }
 
 // NewGW2Api returns a simple GW2Api with a HTTP timeout of 15 seconds
 func NewGW2Api() *GW2Api {
 	api := &GW2Api{
-		client: http.Client{},
+		Client: http.Client{},
 	}
 	api.SetTimeout(time.Duration(15 * time.Second))
 	return api
@@ -46,10 +47,33 @@ func NewAuthenticatedGW2Api(auth string) (api *GW2Api, err error) {
 // SetTimeout set HTTP timeout for all new HTTP connections started from this
 // instance
 func (gw2 *GW2Api) SetTimeout(t time.Duration) {
-	gw2.timeout = t
-	gw2.client.Transport = &http.Transport{
+	gw2.Timeout = t
+	gw2.Client.Transport = &http.Transport{
 		Dial: gw2.dialTimeout,
 	}
+}
+
+// SetAuthentication adds authentication to a previously un-authenticated
+// If permissions is not empty, the api key is assumed to have been working in the past,
+// so it won't waste the limited GW2api calls by checking the apikey with v2/tokeninfo
+// instance of GW2Api
+func (gw2 *GW2Api) SetAuthenticationWithoutCheck(auth string, permissions []string) (err error) {
+	auth = strings.ToUpper(auth)
+	var m bool
+	if m, err = regexp.Match(`^(?:[A-F\d]{4,20}-?){8,}$`, []byte(auth)); !m {
+		return fmt.Errorf("Provided API Key doesn't match expectations: %s", err)
+	}
+
+	gw2.Auth = auth
+	gw2.AuthFlags = 0
+	for _, perm := range permissions {
+		if p, e := permissionsMapping[perm]; e {
+			flagSet(&gw2.AuthFlags, uint(p))
+		} else {
+			log.Print("Found new permission: ", perm)
+		}
+	}
+	return
 }
 
 // SetAuthentication adds authentication to a previously un-authenticated
@@ -60,16 +84,17 @@ func (gw2 *GW2Api) SetAuthentication(auth string) (err error) {
 		return fmt.Errorf("Provided API Key doesn't match expectations: %s", err)
 	}
 
-	gw2.auth = auth
-	gw2.authFlags = 0
+	gw2.Auth = auth
+	gw2.AuthFlags = 0
 	var token TokenInfo
 	if token, err = gw2.TokenInfo(); err != nil {
-		gw2.auth = ""
+		gw2.Auth = ""
 		return fmt.Errorf("Failed to fetch Token Info: %s", err)
 	}
+
 	for _, perm := range token.Permissions {
 		if p, e := permissionsMapping[perm]; e {
-			flagSet(&gw2.authFlags, uint(p))
+			flagSet(&gw2.AuthFlags, uint(p))
 		} else {
 			log.Print("Found new permission: ", perm)
 		}
@@ -79,5 +104,5 @@ func (gw2 *GW2Api) SetAuthentication(auth string) (err error) {
 
 // HasPermission checks for permissions of the API Key provided by the user
 func (gw2 *GW2Api) HasPermission(p Permission) bool {
-	return len(gw2.auth) > 1 && flagGet(gw2.authFlags, uint(p))
+	return len(gw2.Auth) > 1 && flagGet(gw2.AuthFlags, uint(p))
 }
