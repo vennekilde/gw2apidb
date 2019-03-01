7344a6878ea3ec0df6c63cbcedec7078cb204226
Jeppe Vennekilde <jeppe@jeppes.info>
2019-03-01 23:57:47 +0100

    Ignored vscode launch config

--- pkg/gw2api/internal.go
+++ pkg/gw2api/internal.go
@@ -12,12 +12,12 @@ import (
 
 //Timeout solution adapted from Volker on stackoverflow
 func (gw2 *GW2Api) dialTimeout(network, addr string) (net.Conn, error) {
-	return net.DialTimeout(network, addr, gw2.timeout)
+	return net.DialTimeout(network, addr, gw2.Timeout)
 }
 
 func (gw2 *GW2Api) fetchRawEndpoint(url string) (io io.ReadCloser, err error) {
 	var resp *http.Response
-	if resp, err = gw2.client.Get(url); err != nil {
+	if resp, err = gw2.Client.Get(url); err != nil {
 		return
 	}
 	return resp.Body, nil
@@ -32,7 +32,7 @@ func (gw2 *GW2Api) fetchEndpoint(ver, tag string, params url.Values, result inte
 	}
 
 	var resp *http.Response
-	if resp, err = gw2.client.Get(endpoint.String()); err != nil {
+	if resp, err = gw2.Client.Get(endpoint.String()); err != nil {
 		return err
 	}
 	var data []byte
@@ -41,28 +41,32 @@ func (gw2 *GW2Api) fetchEndpoint(ver, tag string, params url.Values, result inte
 	}
 	defer resp.Body.Close()
 
-	if err = json.Unmarshal(data, &result); err != nil {
-		var gwerr APIError
-		if err = json.Unmarshal(data, &gwerr); err != nil {
-			return err
+	if resp.StatusCode == 200 {
+		if err = json.Unmarshal(data, &result); err != nil {
+			var gwerr APIError
+			if err = json.Unmarshal(data, &gwerr); err != nil {
+				return err
+			}
+			return fmt.Errorf("Endpoint returned error: %v", gwerr)
 		}
-		return fmt.Errorf("Endpoint returned error: %v", gwerr)
+	} else {
+		err = fmt.Errorf("GW2API response status code: %d. Body: %s", resp.StatusCode, string(data[:]))
 	}
-	return
+	return err
 }
 
 func (gw2 *GW2Api) fetchAuthenticatedEndpoint(ver, tag string, perm Permission, params url.Values, result interface{}) (err error) {
-	if len(gw2.auth) < 1 {
+	if len(gw2.Auth) < 1 {
 		return fmt.Errorf("API requires authentication")
 	}
 
-	if perm >= PermAccount && !flagGet(gw2.authFlags, uint(perm)) {
+	if perm >= PermAccount && !flagGet(gw2.AuthFlags, uint(perm)) {
 		return fmt.Errorf("Missing permissions for authenticated Endpoint")
 	}
 
 	if params == nil {
 		params = url.Values{}
 	}
-	params.Add("access_token", gw2.auth)
+	params.Add("access_token", gw2.Auth)
 	return gw2.fetchEndpoint(ver, tag, params, result)
 }
