45cb3c017b1f9299398a7c73d9c67a3c896d89d2
Jeppe Vennekilde <jeppe@jeppes.info>
2019-03-03 07:21:57 +0100

    Changed FindLastUpdatedAPIKey to also include keys that has technically expired, but hasn't been processed yet

--- pkg/gw2apidb/auth.go
+++ pkg/gw2apidb/auth.go
@@ -11,6 +11,6 @@ import (
 func FindLastUpdatedAPIKey(ignoreOlderThan int) (tokeninfo gw2api.TokenInfo, err error) {
 	//For some reason, GORM complains if the interval is provided as a paramter. It will say that 1 parameter was provided, when 0 was required.
 	//Anyway, in this case, putting it directly into the query should be fine, as it is an integer
-	result := orm.DB().Order("db_updated").Where("last_success > NOW() - interval '" + strconv.Itoa(ignoreOlderThan) + " seconds'").First(&tokeninfo)
+	result := orm.DB().Order("db_updated").Where("last_success >= db_updated - interval '" + strconv.Itoa(ignoreOlderThan) + " seconds' OR last_success IS NULL").First(&tokeninfo)
 	return tokeninfo, result.Error
 }
